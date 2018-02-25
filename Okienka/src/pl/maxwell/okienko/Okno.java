569c6adf56c45bdf5e12490a9db4c45b5abc6e76
barklis <barklis@maxwell511.mikrus.ds.pw.edu.pl>
2018-02-25 21:31:16 +0100

    Pierwsze kroki z ogarnięciem środowiska Eclipse i Githuba

---
+++ Okienka/src/pl/maxwell/okienko/Okno.java
@@ -0,0 +1,24 @@
+package pl.maxwell.okienko;
+
+import javax.swing.JFrame;
+
+abstract class Okno extends JFrame 
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Okno()
+	{
+		
+	}
+	public void RysujOkno()
+	{
+		
+	}
+	
+	//Pola:
+	int wysokosc;
+	int szerokosc;
+}
