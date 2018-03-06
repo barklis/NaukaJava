b2bd6ce0bdad602a2e0655d321c3052281e4b82a
barklis <barklis@maxwell511.mikrus.ds.pw.edu.pl>
2018-03-06 04:14:03 +0100

    Modyfikacje poźno rano :( ale są 2/3

--- Lab3/src/lab3/MultiListenerFrame.java
+++ Lab3/src/lab3/MultiListenerFrame.java
@@ -6,13 +6,24 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import lab3.InnerClassListenerFrame.SliderChangeListener;
 
 public class MultiListenerFrame extends JFrame
 {
@@ -20,6 +31,13 @@ public class MultiListenerFrame extends JFrame
 	JPanel srodek;
 	JPanel prawy;
 	int wybranyPanel;
+	JSlider R, G, B;
+	JLabel RLabel, GLabel, BLabel;
+	JCheckBox wybor;
+	boolean negacja;
+	JButton zatwierdz;
+	JRadioButton opcja1, opcja2;
+	boolean restart;
 	
 	public MultiListenerFrame() throws HeadlessException
 	{
@@ -28,9 +46,10 @@ public class MultiListenerFrame extends JFrame
 		this.setSize(640, 480);
 		this.setLayout(new GridLayout(1,3));
 		wybranyPanel=0;
+		negacja = false;
 		
 		lewy = new JPanel();
-		lewy.setBackground(Color.BLUE);
+		lewy.setBackground(Color.red);
 		
 		String[] tytul = {"Lewe okno", "Środkowe okno", "Prawe okno"};
 		JComboBox<String> wyborOkna = new JComboBox<String>(tytul);
@@ -44,76 +63,192 @@ public class MultiListenerFrame extends JFrame
         			switch(tytul) 
         			{
         			  	  case "Lewe okno":
-        			  		  lewy.setBackground(Color.green);
+        			  		  //lewy.setBackground(Color.red);
+        			  		  //srodek.setBackground(Color.green);
+        			  		 // prawy.setBackground(Color.green);
         			  		  wybranyPanel=0;
         			  		  break;
         			  	  case "Środkowe okno":
-        			  		  lewy.setBackground(Color.red);
+        			  		 // lewy.setBackground(Color.green);
+        			  		 // srodek.setBackground(Color.red);
+        			  		 // prawy.setBackground(Color.green);
         			  		  wybranyPanel=1;
         			  		  break;
         			  	  case "Prawe okno":
-        			  		  lewy.setBackground(Color.blue);
+        			  		 // lewy.setBackground(Color.green);
+        			  		 // srodek.setBackground(Color.green);
+        			  		 // prawy.setBackground(Color.red);
         			  		  wybranyPanel=2;
         			  		  break;
         			 }
         		}
             	
             }
-        }); //Implementacja pozwalająca na przekazanie informacji o wybranym Panelu
-        
+        }); //Implementacja pozwalająca na przekazanie informacji o wybranym Panelu 
 		this.add(lewy);
 		
 		srodek = new JPanel();
-		srodek.setBackground(Color.RED);
-		JSlider R = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		srodek.setBackground(Color.green);
+		R = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		R.addChangeListener(new RChangeListener());
 		srodek.add(R, BorderLayout.PAGE_START);
-		JSlider G = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		RLabel = new JLabel(String.format("Red: %d", R.getValue()));
+		srodek.add(RLabel);
+		
+		G = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		G.addChangeListener(new GChangeListener());
 		srodek.add(G, BorderLayout.PAGE_START);
-		JSlider B = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		GLabel = new JLabel(String.format("Green: %d", G.getValue()));
+		srodek.add(GLabel);
+		
+		B = new JSlider(JSlider.HORIZONTAL, 0, 255, 100);
+		B.addChangeListener(new BChangeListener());
 		srodek.add(B, BorderLayout.PAGE_START);
+		BLabel = new JLabel(String.format("Blue: %d", B.getValue()));
+		srodek.add(BLabel);
+		
+		wybor = new JCheckBox("Negacja");
+		wybor.addActionListener(new CheckListener(wybor, this));
+		srodek.add(wybor);
+		
 		this.add(srodek);
 		
 		prawy = new JPanel();
-		prawy.setBackground(Color.GREEN);
+		prawy.setBackground(Color.blue);
+		opcja1 = new JRadioButton("Restart");
+		opcja1.addActionListener(new RadioListener(this, opcja1));
+		opcja2 = new JRadioButton("Praca");
+		//opcja2.addActionListener(new RadioListener(this, opcja2));
+		ButtonGroup grupaRadio = new ButtonGroup();
+		grupaRadio.add(opcja1);
+		grupaRadio.add(opcja2);
+		prawy.add(opcja1);
+		prawy.add(opcja2);
+		zatwierdz = new JButton("Zatwierdź");
+		zatwierdz.addActionListener(guzikZatwierdz); 
+		prawy.add(zatwierdz);
+		
 		this.add(prawy);
 	}
 	
-	public int getWybranyPanel() {
-		return wybranyPanel;
-	}
-
-
-	public void setWybranyPanel(int wybranyPanel) {
-		this.wybranyPanel = wybranyPanel;
+	ActionListener guzikZatwierdz = new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{ 
+			if(restart)
+			{
+				lewy.setBackground(Color.red);
+		  		srodek.setBackground(Color.green);
+		  		prawy.setBackground(Color.blue);
+			
+			}
+			else
+			{
+				Color nowy = new Color(R.getValue(), G.getValue(), B.getValue());
+				 switch(wybranyPanel)
+				 {
+				  case 0:
+					  lewy.setBackground(nowy);
+					  break;
+				  case 1:
+					  srodek.setBackground(nowy);
+					  break;
+				  case 2:
+					  prawy.setBackground(nowy);
+					  break;
+				 }
+			}
+			  
+		}
+	};
+	
+	public class RChangeListener implements ChangeListener{
+		
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			String value = String.format("Red: %d", R.getValue());
+			RLabel.setText(value);
+		}
+		
 	}
 	
-	/*@Override
-	public void itemStateChanged(ItemEvent e) {
-		if(e.getStateChange()==ItemEvent.SELECTED) {
-			String tytul = (String) e.getItem();
-			  switch(tytul) {
-			  	  case "Lewe okno":
-			  		  this.setBackground(Color.green);
-			  		  wybranyPanel=0;
-			  		  break;
-			  	  case "Środkowe okno":
-			  		  this.setBackground(Color.red);
-			  		wybranyPanel=1;
-			  		  break;
-			  	  case "Prawe okno":
-			  		  this.setBackground(Color.blue);
-			  		wybranyPanel=2;
-			  		  break;
-			  }
+	public class GChangeListener implements ChangeListener{
+		
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			String value = String.format("Green: %d", G.getValue());
+			GLabel.setText(value);
 		}
+		
+	}
 
-	}*/
+	public class BChangeListener implements ChangeListener{
+	
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			String value = String.format("Blue: %d", B.getValue());
+			BLabel.setText(value);
+		}
+	
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MultiListenerFrame okno = new MultiListenerFrame();
 		okno.setVisible(true);
 	}
+
+
+
+	public JCheckBox getWybor() {
+		return wybor;
+	}
+
+
+
+	public void setWybor(JCheckBox wybor) {
+		this.wybor = wybor;
+	}
+
+
+
+	public boolean isNegacja() {
+		return negacja;
+	}
+
+
+
+	public void setNegacja(boolean negacja) {
+		this.negacja = negacja;
+	}
+
+
+
+	public JButton getZatwierdz() {
+		return zatwierdz;
+	}
+
+
+
+	public void setZatwierdz(JButton zatwierdz) {
+		this.zatwierdz = zatwierdz;
+	}
+
+
+
+	public boolean isRestart() {
+		return restart;
+	}
+
+
+
+	public void setRestart(boolean restart) {
+		this.restart = restart;
+	}
+	
+	
 
 }
 
